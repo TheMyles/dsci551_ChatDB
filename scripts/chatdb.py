afa97fe237305d70c3d4fec9e3cdc8716c727324
MylesMolnar <mdmolnar@usc.edu>
2024-11-18 21:05:29 -0800

    Updates

--- scripts/chatdb.py
+++ scripts/chatdb.py
@@ -1,8 +1,42 @@
 import pymysql
 import pymongo
+import sys
+
+"""
+Setup MySQL DB
+main py file
+Functions in secondary py files
+    NLP
+    [Uploading
+    Generating Example queries
+    Generating queries] * 2 
+"""
+
+mongo_username = 'mdmolnar'
+mongo_password = 'AtM0nG0d1452'
+
+memory = []
+
+def chatdb():
+    continue_running = True
+
+    while continue_running:
+        reponse = ''
+        query = input("Query: ")
+        
+        keywords = (query)
+
+        if 'example' in keywords:
+            response = (keywords)
+
+
+        print(reponse)
 
 
 
+    return  
 
 
+
+chatdb()
 
