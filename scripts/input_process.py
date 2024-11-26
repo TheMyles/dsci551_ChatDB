74bca6ab7138d14d63a04f8689b14692b8772028
MylesMolnar <mdmolnar@usc.edu>
2024-11-25 22:26:12 -0800

    Updates

--- scripts/input_process.py
+++ scripts/input_process.py
@@ -18,7 +18,7 @@ QUERY_PATTERNS = {
                  "sequence", "order", "hierarchy", "top", "bottom", "sorted", "biggest"],
     "WHERE": ["where", "condition", "filter", "over", "under", "less", "greater", "who",
               "which", "that", "criteria", "subset", "find", "limit", "restrict", "search",
-              "match", "constraint"],
+              "match", "constraint", 'fewer', 'below', 'equal', 'equals', 'matches'],
     "JOIN": ["join", "merge", "combine", "foreign key", "unite", "relate", "link",
              "connect", "associate", "bridge", "union", "inner", "outer", "left", "right"],
     "LIMIT": ['top', 'bottom', 'highest', 'lowest', 'biggest', 'smallest', 'limit'],
